74c0c1a7f449756126ac48bdd0b35d754e0e8333
Gavriel Sembiring 366 <gavrielsembiring366@gmail.com>
2023-11-18 15:59:24 +0700

    show using card, and input using form

--- init_db.py
+++ init_db.py
@@ -1,6 +1,16 @@
-
 from main import app, Inputs
 from main import db
+import datetime
 
-app.app_context().push()
-db.create_all()
+# with app.app_context():
+#     db.drop_all()
+
+# app.app_context().push()
+# db.create_all()
+
+with app.app_context():
+     kerusakan = Inputs("idedacsdaswd", "jenis", "bagaian", datetime.date(2015,3,12), datetime.time(9,10,23))
+     db.session.add(kerusakan)
+
+     equipments = Inputs.query.all()
+     print(equipments)
--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import os
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 # from flask_migrate import Migrate
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(
+    basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
 
 class Inputs(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nama_mesin = db.Column(db.Text(200), nullable=False)
-    jenis_rusak = db.Column(db.Text(200), nullable=False)
 
-    def __init__(self,nama_mesin,jenis_rusak):
-        self.nama_mesin = nama_mesin
-        self.jenis_rusak = jenis_rusak
+    id_mesin = db.Column(db.Text(200), nullable=False)
+    jenis_pekerjaan = db.Column(db.Text(200), nullable=False)
+    bagian = db.Column(db.Text(200), nullable=False)
+    tanggal_lapor = db.Column(db.Date, nullable=False)
+    jam_lapor = db.Column(db.Time, nullable=False)
+
+    def __init__(self, id_mesin, jenis_pekerjaan, bagian, tanggal_lapor, jam_lapor):
+        self.id_mesin = id_mesin
+        self.jenis_pekerjaan = jenis_pekerjaan
+        self.bagian = bagian
+        self.tanggal_lapor = tanggal_lapor
+        self.jam_lapor = jam_lapor
 
     def __repr__(self):
         return f"Task added Succesfullys {self.nama_mesin}"
@@ -46,24 +55,31 @@ def dashboard():
     return render_template('dashboard.html', equipments=equipments)
 
 
-
 @app.route('/dashboard', methods=['GET', 'POST'])
 def masuk():
+
     id_mesin = request.form.get('id_mesin')
-    nama_mesin = request.form.get('nama_mesin')
-    
-    kerusakan = Inputs(id_mesin, nama_mesin)
+    jenis_pekerjaan = request.form.get('jenis_pekerjaan')
+    bagian = request.form.get('bagian')
+    tanggal_lapor = request.form.get('tanggal_lapor')
+    jam_lapor = request.form.get('jam_lapor')
+
+    tanggal = datetime.datetime.strptime(tanggal_lapor, '%Y-%m-%d').date()
+    jam = datetime.datetime.strptime(jam_lapor, '%H:%M').time()
+    kerusakan = Inputs(id_mesin, jenis_pekerjaan, bagian, tanggal, jam)
+
     try:
-        db.session.add(kerusakan)
-        db.session.commit()
+        with app.app_context():
+            db.session.add(kerusakan)
+            db.session.commit()
 
         equipments = Inputs.query.all()
-        
+
         return render_template('dashboard.html', equipments=equipments)
-    
+
     except:
         return "There Is an issue adding it"
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
